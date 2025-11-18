5db16df74ad5cfa76d308e2383b4f46146734cbc
matin <150115043+iiMattheww@users.noreply.github.com>
2025-11-19 03:21:03 +0330

    add Hamberger menu

--- src/script/main.js
+++ src/script/main.js
@@ -5,15 +5,15 @@ const input = document.getElementById('link-input')
 const linkForm = document.getElementById('link-form')
 const errMsg = document.getElementById('err-msg')
 
-// btn.addEventListener('click', navToggle)
+btn.addEventListener('click', navToggle)
 linkForm.addEventListener('submit', formSubmit)
 
-// // Toggle Mobile Menu
-// function navToggle() {
-//   btn.classList.toggle('open')
-//   menu.classList.toggle('flex')
-//   menu.classList.toggle('hidden')
-// }
+// Toggle Mobile Menu
+function navToggle() {
+  btn.classList.toggle('open')
+  menu.classList.toggle('flex')
+  menu.classList.toggle('hidden')
+}
 
 // Validate a URL
 function validURL(str) {
